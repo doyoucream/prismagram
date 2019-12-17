5e073e3a12aab3c7185fd96f009dc7595955b1b5
dollydev <dollydev@dollyui-MacBook-Pro.local>
2019-12-17 13:04:19 +0900

    done 3.5

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# prismagram
+# Prismagram
+
 Instagram clone with Express + Prisma + React and React Native
+
+## User Stories
+
+- [x] Create account
+- [x] Request Secret
+- [x] Confirm Secret (Login)
+- [ ] Like / Unlike a photo
+- [ ] Comment on a photo
+- [ ] Search by user
+- [ ] Search by location
+- [ ] See user profile
+- [ ] Follow / Unfollow User
+- [ ] See the full photo
+- [ ] Edit my profile
+- [ ] Upload a photo
+- [ ] Edit the photo (Delete)
+- [ ] See the feed
+
+yarn add nodemon -D
+yarn add babel-cli -D
+yarn add dotenv
+yarn add @babel/preset-env@^7.4.5
+yarn add @babel/core@^7.4.5
+yarn add @babel/node@^7.4.5
+yarn add babel-cli@7.0.0
+yarn add morgan
+yarn add graphql-tools
+yarn add merge-graphql-schemas
+yarn add graphql-yoga
+yarn global add prisma
+yarn add nodemailer
+yarn add nodemailer-mailgun-transport
+yarn add passport
+yarn add passport-jwt
+yarn add jsonwebtoken
+
+git add .
+git status
+git commit -m "env edit"
+git push origin master
---
+++ src/utils.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+import { adjectives, verbs } from "./words";
+import nodemailer from "nodemailer";
+import mg from "nodemailer-mailgun-transport";
+import jwt from "jsonwebtoken";
+
+export const generateSecret = () => {
+    const randomNumber = Math.floor(Math.random() * adjectives.length);
+    return `${adjectives[randomNumber]} ${verbs[randomNumber]}`;
+};
+
+const sendMail = email => {
+    const options = {
+        auth: {
+                api_key: process.env.MAILGUN_API_KEY,
+                domain: process.env.MAILGUN_DOMAIN
+            }
+    }
+    const client = nodemailer.createTransport(mg(options));
+    return client.sendMail(email);
+};
+
+export const sendSecretMail = ( address, secret ) => {
+    const email = {
+        from: "doyoucream@prismagram.com",
+        to: address,
+        subject: "Login Secret for Prismagram",
+        html: `Hello, Your login secret is <strong>${secret}</strong>.<br/>Copy and Paste on the app/website to log in.`
+    }
+    return sendMail(email);
+}
+
+export const generateToken = ( id ) => jwt.sign({ id }, process.env.JWT_SECRET);
